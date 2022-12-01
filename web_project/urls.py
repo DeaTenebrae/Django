07f8b67da4dcd2efc4f9184508fe5b71139e161c
DeaTenebrae <dea.tenebrae@gmail.com>
2022-12-01 08:42:36 +0100

    update

--- web_project/urls.py
+++ web_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("hello.urls")),
-    path('admin/', admin.site.urls)
+    path("", include("hello.urls")), # root-pfad 
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')), # wenn users Verzeichnis aufgerufen wird, soll users.urls geöffnet werden
 ]
